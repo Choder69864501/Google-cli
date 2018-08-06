319855b609c646860b9682b5c39076cbd07de193
Choder <choder69864501@gmail.com>
2018-08-06 22:41:34 +0800

    add ValueError and KeyboardInterrupt exception

--- Google.py
+++ Google.py
@@ -1,11 +1,15 @@
 import requests
 import webbrowser
+import sys
 from bs4 import BeautifulSoup
 headers = {}
 headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 url = 'https://www.google.com/search'
 print('Search:', end='', flush=True)
-Q = input()
+try:
+    Q = input()
+except KeyboardInterrupt:
+    sys.exit()
 print('')
 data = {'q': Q}
 soup = requests.get(url, data, headers=headers)
@@ -18,6 +22,10 @@ for link in soup.find_all('h3', 'r'):
     sugs.append(temp['href'])
     a += 1
 while True:
-    Choose = input('Choose one:')
-    Choose = int(Choose) - 1
+    try:
+        Choose = input('Choose one:')
+        Choose = int(Choose) - 1
+    except (KeyboardInterrupt, ValueError):
+        break
     webbrowser.open_new_tab(sugs[Choose])
+sys.exit()
