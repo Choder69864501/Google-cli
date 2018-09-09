d9dca1fe4eb0de904cb620a88b5d607001de6ea8
Choder <Choder69864501@gmail.com>
2018-09-09 22:52:23 +0800

    pep8 style

--- Google.py
+++ Google.py
@@ -6,15 +6,20 @@ headers = {}
 headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 url = 'https://www.google.com/search'
 
+
 def google():
     global Q
-    print("Search or input website(Press ctrl-c to exit):", end='\n--->', flush=True)
+    print(
+        "Search or input website(Press ctrl-c to exit):",
+        end='\n--->',
+        flush=True)
     try:
         Q = input()
     except KeyboardInterrupt:
         print('')
         sys.exit()
-    if Q.split('.')[0]=='www' or Q.split('.')[0]=='https://www' or Q.split('.')[0]=='http://www':
+    if Q.split('.')[0] == 'www' or Q.split('.')[
+            0] == 'https://www' or Q.split('.')[0] == 'http://www':
         print('Opening website')
         return None
     print('')
@@ -28,9 +33,9 @@ while True:
     soup = google()
     a = 1
     sugs = []
-    if soup==None:
-       webbrowser.open_new_tab(Q) 
-       continue
+    if soup is None:
+        webbrowser.open_new_tab(Q)
+        continue
     for link in soup.find_all('h3', 'r'):
         temp = link.find('a')
         print(a, temp.text)
