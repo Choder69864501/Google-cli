b3aee2505606cd2a8f715739eeac7f365eeeff4f
Choder <Choder69864501@gmail.com>
2018-09-09 22:49:40 +0800

    add open website feature

--- Google.py
+++ Google.py
@@ -6,14 +6,17 @@ headers = {}
 headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 url = 'https://www.google.com/search'
 
-
 def google():
-    print("Search(Press ctrl-c to exit):", end='', flush=True)
+    global Q
+    print("Search or input website(Press ctrl-c to exit):", end='\n--->', flush=True)
     try:
         Q = input()
     except KeyboardInterrupt:
         print('')
         sys.exit()
+    if Q.split('.')[0]=='www' or Q.split('.')[0]=='https://www' or Q.split('.')[0]=='http://www':
+        print('Opening website')
+        return None
     print('')
     data = {'q': Q}
     soup = requests.get(url, data, headers=headers)
@@ -25,6 +28,9 @@ while True:
     soup = google()
     a = 1
     sugs = []
+    if soup==None:
+       webbrowser.open_new_tab(Q) 
+       continue
     for link in soup.find_all('h3', 'r'):
         temp = link.find('a')
         print(a, temp.text)
